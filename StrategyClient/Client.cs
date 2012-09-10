1bb02e3ad0ab4d601fc6dd19997ba8b5b2b4ff76
Jiří Bobrek <bobrekjiri@gmail.com>
2012-09-10 23:10:43 +0200

    Updated Encryption
    Created Encryptor class for easier work with encryptions.
    I have found another encryption bug, its solved now. I guess this was
    the last one.

--- StrategyClient/Client.cs
+++ StrategyClient/Client.cs
@@ -11,17 +11,22 @@ namespace StrategyClient
 {
     class Client : IDisposable
     {
+        public event EventHandler Connected;
+        public event EventHandler AnswerReceived;
+
         private TcpClient client;
-        NetworkStream clientStream;
-        private AesManaged aes;
-        private ICryptoTransform encryptor;
-        private ICryptoTransform decryptor;
+        private NetworkStream clientStream;
+        private Encryptor encryptor;
+
         public Client()
+        {
+        }
+
+        public void Connect()
         {
             IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("213.220.250.46"), 9020); //TODO: load both values from configuration file
             client = new TcpClient();
             client.Connect(serverEndPoint);
-
             clientStream = client.GetStream();
 
             UTF8Encoding encoder = new UTF8Encoding();
@@ -31,42 +36,26 @@ namespace StrategyClient
             string key = encoder.GetString(buffer, 0, length);
             RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
             rsa.FromXmlString(key);
-            aes = new AesManaged();
-            encryptor = aes.CreateEncryptor();
-            decryptor = aes.CreateDecryptor();
-            buffer = new byte[48];
-
-            Array.Copy(aes.IV, buffer, 16);
-            Array.Copy(aes.Key, 0, buffer, 16, 32);
+            encryptor = new Encryptor();
+            buffer = encryptor.ExportKey();
             buffer = rsa.Encrypt(buffer, true);
             clientStream.Write(buffer, 0, buffer.Length);
+
+            OnConnected(new EventArgs());
         }
 
-        private byte[] Encrypt(string text)
+        protected virtual void OnConnected(EventArgs e)
         {
-            using (MemoryStream msEncrypt = new MemoryStream())
+            if (Connected != null)
             {
-                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                {
-                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                    {
-                        swEncrypt.Write(text);
-                    }
-                }
-                return msEncrypt.ToArray();
+                Connected(this, e);
             }
         }
-        private string Decrypt(byte[] buffer)
+        protected virtual void OnAnswerReceived(EventArgs e)
         {
-            using (MemoryStream msDecrypt = new MemoryStream(buffer))
+            if (AnswerReceived != null)
             {
-                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                {
-                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                    {
-                        return srDecrypt.ReadToEnd();
-                    }
-                }
+                AnswerReceived(this, e);
             }
         }
 
@@ -75,16 +64,18 @@ namespace StrategyClient
             string text = (short)type + "~";
             text += data;
 
-            byte[] buffer = Encrypt(text);
-            string text2 = Decrypt(buffer);
+            byte[] buffer = encryptor.Encrypt(text);
             clientStream.Write(buffer, 0, buffer.Length);
 
             buffer = new byte[4096];
             int length = clientStream.Read(buffer, 0, buffer.Length);
             byte[] buffer2 = new byte[length];
-            string messsage = Decrypt(buffer2);
-            short answerType = (short)messsage[0];
+            Array.Copy(buffer, buffer2, length);
+            string messsage = encryptor.Decrypt(buffer2);
+
             //TODO: handle answer
+
+            OnAnswerReceived(new EventArgs());
         }
 
         public void Dispose()
